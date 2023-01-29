260fa55a6ac4fdf183dd93998fd5aedafe869d65
maneesh29s <sutarmaneesh00@gmail.com>
2023-01-29 15:36:20 +0530

    Maneesh : Created seperate methods for reading and writing to binary data

--- test/test_array_slicer.cc
+++ test/test_array_slicer.cc
@@ -10,9 +10,49 @@
 #include "CasaImageAccess.h"
 #include "helper.h"
 
-static void writeData(const int &naxes, const std::vector<size_t> &naxis)
-{
+static void writeDataBinary(const std::vector<size_t> &naxis){
+    std::ofstream writer;
+    writer.open("test/test_array_data.dat");
+    
+    size_t naxes = naxis.size();
 
+    // std::cout << "naxes: " << naxes << std::endl;
+    writer.write((char *)&naxes, sizeof(size_t));
+
+    for (size_t i = 0; i < naxes; i++)
+    {
+        // std::cout << "naxis" << i << ": " << naxis[i] << std::endl;
+        writer.write((char *)&naxis[i], sizeof(size_t));
+    }
+
+    size_t totpix = 1;
+    for (size_t i = 0; i < naxes; i++)
+    {
+        totpix *= naxis[i];
+    }
+
+    time_t seed = time(0);
+    // std::cout << "Seed: " << seed << std::endl;
+    srand(seed);
+
+    // std::cout << "Array dim : " << totpix << std::endl;
+
+    float temp;
+    for (size_t i = 0; i < totpix; i++)
+    {
+        temp = i + 100;
+        writer.write((char *)&temp, sizeof(float));
+    }
+
+    std::cout << "Array creation done" << std::endl;
+
+    writer.close();
+}
+
+static void writeData(const std::vector<size_t> &naxis)
+{
+    size_t naxes = naxis.size();
+    
     size_t totpix = 1;
     for (int i = 0; i < naxes; i++)
     {
@@ -20,14 +60,10 @@ static void writeData(const int &naxes, const std::vector<size_t> &naxis)
     }
 
     time_t seed = time(0);
-    std::cout << "Seed: " << seed << std::endl;
+    // std::cout << "Seed: " << seed << std::endl;
     srand(seed);
 
-    std::cout << "Array dim : " << totpix << std::endl;
-
-    float offset = -5.0f;
-    float range = 5.0f;
-    float temp;
+    // std::cout << "Array dim : " << totpix << std::endl;
 
     casacore::IPosition arrSize(naxes, naxis[0], naxis[1], naxis[2], naxis[3]);
     casacore::Array<casacore::Float> arr(arrSize);
@@ -37,7 +73,7 @@ static void writeData(const int &naxes, const std::vector<size_t> &naxis)
         for (size_t col = 0; col < naxis[1]; col++)
         {
             casacore::IPosition currentPos(naxes, row, col, 0, 0);
-            arr(currentPos) = row * naxis[1] + col;
+            arr(currentPos) = row * naxis[1] + col + 100;
         }
     }
 
@@ -50,6 +86,104 @@ static void writeData(const int &naxes, const std::vector<size_t> &naxis)
     accessor.write("test/casa_test_image.FITS", arr);
 
     std::cout << "Array creation done" << std::endl;
+}
+
+static void readDataBinary(){
+    Json::Reader jsonReader; // for reading the data
+    Json::Value root;        // for modifying and storing new values
+
+    std::ifstream dataFile;
+    dataFile.open("test/test_array_data.dat");
+    std::ifstream jsonFile;
+    jsonFile.open("test/test_log.json");
+
+    // check if there is any error is getting data from the json jsonFile
+    if (!jsonReader.parse(jsonFile, root, false))
+    {
+        std::cerr << jsonReader.getFormattedErrorMessages();
+        exit(1);
+    }
+
+    size_t naxes;
+    dataFile.read((char *)&naxes, sizeof(size_t));
+
+    // for current application, naxes must be 4
+    if (naxes != 4)
+    {
+        std::cerr << "This application requires a 4D array. Please recreate array using array_creator.";
+        exit(1);
+    }
+
+    std::vector<size_t> naxis(naxes);
+
+    for (size_t i = 0; i < naxes; i++)
+    {
+        dataFile.read((char *)&naxis[i], sizeof(size_t));
+    }
+
+    // reading whole data
+    casacore::IPosition arrSize(naxes, naxis[0], naxis[1], naxis[2], naxis[3]);
+    casacore::Array<casacore::Float> arr(arrSize);
+
+    std::cout << "Original Array" << std::endl;
+    float value;
+    for (size_t row = 0; row < naxis[0]; row++)
+    {
+        for (size_t col = 0; col < naxis[1]; col++)
+        {
+            casacore::IPosition currentPos(naxes, row, col, 0, 0);
+            dataFile.read((char *)&value, sizeof(float));
+            std::cout << value << " ";
+            arr(currentPos) = value;
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+
+    for (Json::Value::ArrayIndex i = 0; i != root.size(); i++)
+    {
+        int sourceID = root[i]["sourceID"].asInt();
+
+        casacore::Vector<casacore::Int64> slicerBegin(naxes);
+        casacore::Vector<casacore::Int64> slicerEnd(naxes);
+        casacore::Vector<casacore::Int64> stride(naxes);
+        casacore::Vector<casacore::Int64> length(naxes);
+
+        for (Json::Value::ArrayIndex j = 0; j < naxes; j++)
+        {
+            slicerBegin(j) = root[i]["slicerBegin"][j].asInt64();
+            slicerEnd(j) = root[i]["slicerEnd"][j].asInt64();
+            stride(j) = root[i]["stride"][j].asInt64();
+            length(j) = root[i]["length"][j].asInt64();
+        }
+
+        casacore::IPosition blc(slicerBegin);
+        casacore::IPosition trc(slicerEnd);
+
+        casacore::Slicer slicer = casacore::Slicer(blc, trc, casacore::Slicer::endIsLast);
+        casacore::Array<casacore::Float> output = arr(slicer);
+
+        std::cout << "Sliced array dimensions :" << std::endl;
+        std::cout << "numelements : " << output.size() << std::endl;
+        std::cout << "numdimensions : " << output.ndim() << std::endl;
+        std::cout << "sourceID : " << sourceID << std::endl;
+        std::cout << std::endl;
+
+        std::cout << "Sliced Array" << std::endl;
+        for (size_t row = 0; row < output.shape()(0); row++)
+        {
+            for (size_t col = 0; col < output.shape()(1); col++)
+            {
+                casacore::IPosition currentPos(naxes, row, col, 0, 0);
+                std::cout << output(currentPos) << " ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+    }
+
+    dataFile.close();
+    jsonFile.close();
 }
 
 static void readData()
@@ -204,15 +338,17 @@ static void readDataSliced()
 
 int main(int argc, char const *argv[])
 {
-    int naxes = 4;
+    // size_t naxes = 4;
     std::vector<size_t> naxis{10, 10, 1, 1};
-
-    //TODO: Create seperate read and write function for binary data
 
     Timer timer;
 
+    writeDataBinary(naxis);
+    readDataBinary();
+
+
     timer.start_timer();
-    writeData(naxes, naxis);
+    writeData(naxis);
     timer.stop_timer();
 
     std::cerr << "Time taken for writing: " << timer.time_elapsed() << " us" << std::endl;
