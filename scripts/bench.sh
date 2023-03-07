a5b753bcf66ffa4c6b58a7f08ae7afd3ff8e4fdc
maneesh29s <sutarmaneesh00@gmail.com>
2023-03-07 15:30:18 +0530

    Maneesh / Ravinder: Fixed passing flags issue with bench.sh

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -13,27 +13,27 @@ outputFile="benchmarks-`TZ=Asia/Kolkata date +%Y-%m-%dT%H:%M`"
 # Cubelet Extractor (2D)
 echo "Cubelet JSON File being processed: ${cubeletJsonFileSorted}" >> ${outputFile}
 # fits to fits - single read for whole data
-make -DSINGLE_READ
+make build/bench_cubelet_extractor_2d.out OPT="-DSINGLE_READ"
 ./build/bench_cubelet_extractor_2d.out fits fits ${cubeletJsonFileSorted} >> ${outputFile}
 # fits to bp - single read for whole data
 ./build/bench_cubelet_extractor_2d.out fits bp ${cubeletJsonFileSorted} >> ${outputFile}
 
 # fits to fits - sliced read (source by source)
-make -DSLICED_READ
+make build/bench_cubelet_extractor_2d.out OPT="-DSLICED_READ"
 ./build/bench_cubelet_extractor_2d.out fits fits ${cubeletJsonFileSorted} >> ${outputFile}
 # fits to bp - sliced read (source by source)
 ./build/bench_cubelet_extractor_2d.out fits bp ${cubeletJsonFileSorted} >> ${outputFile}
 
 # Spectral Extractor (3D)
 echo "Spectral JSON File being processed: ${spectralJsonFileSorted}" >> ${outputFile}
-# fits to fits - sorted-grouped reads 
-make -DSORTED_GROUPED_READ
+# fits to fits - sorted-grouped reads
+make build/bench_spectrum_extractor_3d.out OPT="-DSORTED_GROUPED_READ"
 ./build/bench_spectrum_extractor_3d.out fits fits ${spectralJsonFileSorted} >> ${outputFile}
-# fits to bp - sorted-grouped reads 
+# fits to bp - sorted-grouped reads
 ./build/bench_spectrum_extractor_3d.out fits bp ${spectralJsonFileSorted} >> ${outputFile}
 
 # fits to fits - sliced read (source by source)
-make -DSLICED_READ
+make build/bench_spectrum_extractor_3d.out OPT="-DSLICED_READ"
 ./build/bench_spectrum_extractor_3d.out fits fits ${spectralJsonFileSorted} >> ${outputFile}
 # fits to bp - sliced read (source by source)
 ./build/bench_spectrum_extractor_3d.out fits bp ${spectralJsonFileSorted} >> ${outputFile}
