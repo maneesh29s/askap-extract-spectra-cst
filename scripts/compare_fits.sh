2063d8ddc17b9152b29804cf92376ea4cd94cf73
maneesh29s <sutarmaneesh00@gmail.com>
2023-03-07 18:35:11 +0530

    Maneesh / Ravinder: Added logic to compare spectral fits files

--- scripts/compare_fits.sh
+++ scripts/compare_fits.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-for path in test_data/fits-to-fits_single_read_output_sources/*
+for path in test_data_3d/fits-to-fits_multi_read_output_sources/*
 do
     imageName=`basename $path`
-    fitsdiff -n 100 test_data/fits-to-fits_multi_read_output_sources/$imageName test_data/fits-to-fits_single_read_output_sources/$imageName
+    fitsdiff -n 100 test_data_3d/fits-to-fits_multi_read_output_sources/$imageName test_data_3d/fits-to-fits_sorted_grouped_read_output_sources/$imageName
 done
+
+# for path in test_data_2d/fits-to-fits_multi_read_output_sources/*
+# do
+#     imageName=`basename $path`
+#     fitsdiff -n 100 test_data_2d/fits-to-fits_multi_read_output_sources/$imageName test_data_2d/fits-to-fits_single_read_output_sources/$imageName
+# done
